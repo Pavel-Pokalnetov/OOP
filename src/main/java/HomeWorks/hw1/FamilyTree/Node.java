a5968c65a7419bff745a83ea3c6852e89a0ea380
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-03-03 14:03:53 +0500

    hw1 #1

--- src/main/java/HomeWorks/hw1/FamilyTree/Node.java
+++ src/main/java/HomeWorks/hw1/FamilyTree/Node.java
@@ -2,9 +2,7 @@ package HomeWorks.hw1.FamilyTree;
 
 public class Node {
 
-    public Person getP2() {
-        return p2;
-    }
+
 
     public Node(Person person, Relationship re, Person relative) {
         this.p1 = person;
@@ -15,19 +13,22 @@ public class Node {
     public Person getP1() {
         return p1;
     }
-
+    public Person getP2() {
+        return p2;
+    }
     public Relationship getRe() {
         return re;
     }
 
-    private Person p1;//персона
-    private Relationship re; //родственное отношение
-    private Person p2;//родственник
+    private final Person p1;//персона
+    private final Relationship re; //родственное отношение
+    private final Person p2;//родственник
 
     @Override
     public String toString() {
         return String.format("<%s %s %s>", p1, re, p2);
     }
+
 
 
 }
