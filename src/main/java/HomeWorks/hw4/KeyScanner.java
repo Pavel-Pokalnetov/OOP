709f528b98464ea664ccf1d26566a706ef7fdf11
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-03-16 13:14:12 +0500

    fix2

---
+++ src/main/java/HomeWorks/hw4/KeyScanner.java
@@ -0,0 +1,10 @@
+package HomeWorks.hw4;
+
+import java.util.Scanner;
+
+public class KeyScanner {
+    public static Scanner scanner = new Scanner(System.in);
+    public static String getText(){
+        return scanner.next();
+    }
+}
--- src/main/java/HomeWorks/hw4/Main.java
+++ src/main/java/HomeWorks/hw4/Main.java
@@ -2,6 +2,22 @@ package HomeWorks.hw4;
 
 public class Main {
     public static void main(String[] args) {
-        Scheduler scheduler = new Scheduler().tasksLoad("schedulerBase.dat");
+
+        Menu menu = new Menu("Главное меню");
+        menu.addStop("0","Выход");
+        menu.add("1","Вывод всех записей",(Runnable)() -> task1());
+        menu.add("2","Вывод всех записей",(Runnable)() -> task2());
+        menu.run();
+
+
     }
+
+    public static void task1(){
+        System.out.println("задача №1");
+    }
+    public static void task2(){
+        System.out.println("задача №2");
+    }
+
+
 }
